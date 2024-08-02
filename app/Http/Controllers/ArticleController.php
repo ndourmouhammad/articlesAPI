8158d092ee5e4b87a5a2961c761bdbe4bead5a98
ndourmouhammad <ndourmouhammad@gmail.com>
2024-08-02 13:14:36 +0000

    Mise en place du controller de mon api

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -2,47 +2,83 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Article;
 use Illuminate\Http\Request;
 
 class ArticleController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * Renvoie la liste de toutes les articles
      */
     public function index()
     {
-        //
+        return Article::all();
     }
 
     /**
-     * Store a newly created resource in storage.
+     * Enregistre un nouvel article
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'title' => 'required',
+            'body' => 'required',
+            'auteur' => 'required',
+        ]);
+
+        $article = Article::create($request->all());
     }
 
     /**
-     * Display the specified resource.
+     * Afficher un article selon son identifiant
      */
     public function show(string $id)
     {
-        //
+        $article = Article::find($id);
+
+        if (!$article) {
+            return response()->json(['message' => "L'article n'existe pas"], 
+            404);
+        }
+
+        return $article;
     }
 
     /**
-     * Update the specified resource in storage.
+     * Mettre à jour un article
      */
     public function update(Request $request, string $id)
     {
-        //
+        $article = Article::find($id);
+
+        if (!$article) {
+            return response()->json(['message' => "L'article n'existe pas"], 
+            404);
+        }
+
+        $request->validate([
+            'title' => 'required',
+            'body' => 'required',
+            'auteur' => 'required',
+        ]);
+
+        $article->update($request->all());
+        return $article;
     }
 
     /**
-     * Remove the specified resource from storage.
+     * Supprimer un article
      */
     public function destroy(string $id)
     {
-        //
+        $article = Article::find($id);
+
+        if (!$article) {
+            return response()->json(['message' => "L'article n'existe pas"], 
+            404);
+        }
+
+        $article->delete();
+        return response()->json(['message' => "L'article a bien été supprimé"]);
     }
 }
